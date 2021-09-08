dafb7aba570219b0e45486a7d61fc10be0bd6eb9
shilat <shilat199@gmail.com>
2021-09-09 00:30:19 +0300

    update

--- Durak/SignIn.Designer.cs
+++ Durak/SignIn.Designer.cs
@@ -36,6 +36,10 @@ namespace Durak
             this.btnSignIn = new System.Windows.Forms.Button();
             this.radioBtnFemale = new System.Windows.Forms.RadioButton();
             this.radioBtnMale = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // lblNname
@@ -43,9 +47,9 @@ namespace Durak
             this.lblNname.AutoSize = true;
             this.lblNname.BackColor = System.Drawing.Color.Transparent;
             this.lblNname.Font = new System.Drawing.Font("Monotype Corsiva", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNname.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.lblNname.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.lblNname.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.lblNname.Location = new System.Drawing.Point(168, 64);
+            this.lblNname.Location = new System.Drawing.Point(285, 94);
             this.lblNname.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNname.Name = "lblNname";
             this.lblNname.Size = new System.Drawing.Size(215, 49);
@@ -56,41 +60,41 @@ namespace Durak
             // 
             this.lblPicProfile.AutoSize = true;
             this.lblPicProfile.BackColor = System.Drawing.Color.Transparent;
-            this.lblPicProfile.Font = new System.Drawing.Font("Monotype Corsiva", 35.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPicProfile.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.lblPicProfile.Font = new System.Drawing.Font("Monotype Corsiva", 33F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPicProfile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.lblPicProfile.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.lblPicProfile.Location = new System.Drawing.Point(144, 136);
+            this.lblPicProfile.Location = new System.Drawing.Point(275, 157);
             this.lblPicProfile.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPicProfile.Name = "lblPicProfile";
-            this.lblPicProfile.Size = new System.Drawing.Size(434, 56);
+            this.lblPicProfile.Size = new System.Drawing.Size(389, 53);
             this.lblPicProfile.TabIndex = 2;
             this.lblPicProfile.Text = "Choose Picture Profile :";
             // 
             // txtName
             // 
-            this.txtName.BackColor = System.Drawing.Color.AliceBlue;
+            this.txtName.BackColor = System.Drawing.Color.White;
             this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtName.Font = new System.Drawing.Font("Arial Narrow", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtName.Location = new System.Drawing.Point(387, 68);
+            this.txtName.Font = new System.Drawing.Font("Choco", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.txtName.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.txtName.Location = new System.Drawing.Point(495, 103);
             this.txtName.Margin = new System.Windows.Forms.Padding(2);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(173, 40);
+            this.txtName.Size = new System.Drawing.Size(181, 37);
             this.txtName.TabIndex = 3;
             this.txtName.Tag = "";
             // 
             // btnSignIn
             // 
             this.btnSignIn.BackColor = System.Drawing.Color.Black;
-            this.btnSignIn.Font = new System.Drawing.Font("Perpetua", 30F, System.Drawing.FontStyle.Bold);
-            this.btnSignIn.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnSignIn.Font = new System.Drawing.Font("Perpetua", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignIn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.btnSignIn.Image = ((System.Drawing.Image)(resources.GetObject("btnSignIn.Image")));
             this.btnSignIn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnSignIn.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.btnSignIn.Location = new System.Drawing.Point(269, 337);
+            this.btnSignIn.Location = new System.Drawing.Point(386, 334);
             this.btnSignIn.Margin = new System.Windows.Forms.Padding(2);
             this.btnSignIn.Name = "btnSignIn";
-            this.btnSignIn.Size = new System.Drawing.Size(178, 59);
+            this.btnSignIn.Size = new System.Drawing.Size(178, 52);
             this.btnSignIn.TabIndex = 4;
             this.btnSignIn.Text = "Login    ";
             this.btnSignIn.UseVisualStyleBackColor = false;
@@ -102,7 +106,7 @@ namespace Durak
             this.radioBtnFemale.BackColor = System.Drawing.Color.Transparent;
             this.radioBtnFemale.Cursor = System.Windows.Forms.Cursors.Hand;
             this.radioBtnFemale.Image = ((System.Drawing.Image)(resources.GetObject("radioBtnFemale.Image")));
-            this.radioBtnFemale.Location = new System.Drawing.Point(232, 220);
+            this.radioBtnFemale.Location = new System.Drawing.Point(352, 223);
             this.radioBtnFemale.Name = "radioBtnFemale";
             this.radioBtnFemale.Size = new System.Drawing.Size(94, 80);
             this.radioBtnFemale.TabIndex = 7;
@@ -116,13 +120,69 @@ namespace Durak
             this.radioBtnMale.BackColor = System.Drawing.Color.Transparent;
             this.radioBtnMale.Cursor = System.Windows.Forms.Cursors.Hand;
             this.radioBtnMale.Image = ((System.Drawing.Image)(resources.GetObject("radioBtnMale.Image")));
-            this.radioBtnMale.Location = new System.Drawing.Point(387, 220);
+            this.radioBtnMale.Location = new System.Drawing.Point(507, 223);
             this.radioBtnMale.Name = "radioBtnMale";
             this.radioBtnMale.Size = new System.Drawing.Size(94, 80);
             this.radioBtnMale.TabIndex = 8;
             this.radioBtnMale.TabStop = true;
             this.radioBtnMale.UseCompatibleTextRendering = true;
             this.radioBtnMale.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton1.Image = ((System.Drawing.Image)(resources.GetObject("radioButton1.Image")));
+            this.radioButton1.Location = new System.Drawing.Point(352, 223);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(94, 80);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseCompatibleTextRendering = true;
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton2.Image = ((System.Drawing.Image)(resources.GetObject("radioButton2.Image")));
+            this.radioButton2.Location = new System.Drawing.Point(507, 223);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(94, 80);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseCompatibleTextRendering = true;
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton3.Image = ((System.Drawing.Image)(resources.GetObject("radioButton3.Image")));
+            this.radioButton3.Location = new System.Drawing.Point(352, 225);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(94, 80);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseCompatibleTextRendering = true;
+            this.radioButton3.UseVisualStyleBackColor = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton4.Image = ((System.Drawing.Image)(resources.GetObject("radioButton4.Image")));
+            this.radioButton4.Location = new System.Drawing.Point(507, 225);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(94, 80);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseCompatibleTextRendering = true;
+            this.radioButton4.UseVisualStyleBackColor = false;
             // 
             // SignIn
             // 
@@ -132,7 +192,11 @@ namespace Durak
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(730, 457);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioBtnMale);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.radioBtnFemale);
             this.Controls.Add(this.btnSignIn);
             this.Controls.Add(this.txtName);
@@ -160,6 +224,10 @@ namespace Durak
         private System.Windows.Forms.Button btnSignIn;
         private System.Windows.Forms.RadioButton radioBtnFemale;
         private System.Windows.Forms.RadioButton radioBtnMale;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
     }
 }
 
--- Durak/Welcome.Designer.cs
+++ Durak/Welcome.Designer.cs
@@ -40,9 +40,9 @@
             this.prgBar.BackColor = System.Drawing.Color.White;
             this.prgBar.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.prgBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.prgBar.Location = new System.Drawing.Point(0, 464);
+            this.prgBar.Location = new System.Drawing.Point(0, 488);
             this.prgBar.Name = "prgBar";
-            this.prgBar.Size = new System.Drawing.Size(750, 36);
+            this.prgBar.Size = new System.Drawing.Size(750, 12);
             this.prgBar.TabIndex = 0;
             this.prgBar.UseWaitCursor = true;
             // 
@@ -53,12 +53,12 @@
             // lbl1
             // 
             this.lbl1.AutoSize = true;
-            this.lbl1.BackColor = System.Drawing.Color.Black;
-            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl1.Font = new System.Drawing.Font("Lucida Handwriting", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl1.ForeColor = System.Drawing.Color.Red;
-            this.lbl1.Location = new System.Drawing.Point(12, 472);
+            this.lbl1.Location = new System.Drawing.Point(1, 460);
             this.lbl1.Name = "lbl1";
-            this.lbl1.Size = new System.Drawing.Size(66, 24);
+            this.lbl1.Size = new System.Drawing.Size(93, 27);
             this.lbl1.TabIndex = 1;
             this.lbl1.Text = "label1";
             // 
