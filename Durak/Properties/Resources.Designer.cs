9fca5720292ec557b423b62dfa4e2aa54834c51c
shilat <shilat199@gmail.com>
2021-12-20 19:18:43 +0200

    .

--- Durak/Properties/Resources.Designer.cs
+++ Durak/Properties/Resources.Designer.cs
@@ -439,15 +439,5 @@ namespace Durak.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap tableskin {
-            get {
-                object obj = ResourceManager.GetObject("tableskin", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
