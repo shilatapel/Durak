834cdc60105c68e3d0a24b859477fafdf9b4176d
Ivgi Zolcberg <android7119024@mail.ru>
2021-12-29 05:12:26 +0200

    Fixed wrong steps and wrong ending of the game

---
+++ Durak/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Durak
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
