58f87fe43db84e484a62fd4d90f1709690f4b8b0
Ivgi Zolcberg <android7119024@mail.ru>
2021-12-30 23:05:56 +0200

    Added Triangle Custom Button

--- Durak/Classes/TriangleButton.cs
+++ Durak/Classes/TriangleButton.cs
@@ -1,7 +1,84 @@
-﻿namespace Durak.Classes
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Durak.Classes
 {
-    public class TriangleButton
+    public class TriangleButton : Button
     {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TriangleButton
+            // 
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.FlatAppearance.BorderSize = 0;
+            this.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.UseVisualStyleBackColor = false;
+            this.ResumeLayout(false);
+
+        }
+        public TriangleButton()
+        {
+            _angle = 0;
+            _edgesCount = 3;
+        }
+
+        private int _edgesCount;
+        private float _angle;
         
+        
+        
+        public int EdgesCount
+        {
+            get { return _edgesCount; }
+            set
+            {
+                if (_edgesCount == value) return;
+                _edgesCount = value;
+                OnSizeChanged(EventArgs.Empty);
+            }
+        }
+
+        public float Angle
+        {
+            get { return _angle; }
+            set
+            {
+                if (_angle == value) return;
+                _angle = value;
+                OnSizeChanged(EventArgs.Empty);
+            }
+        }
+        
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            OnSizeChanged(e);
+        }
+ 
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            using (var path = new GraphicsPath())
+            {
+                var rect = ClientRectangle;
+                rect.Inflate(-3,-3);
+                var pt = new PointF(rect.X + rect.Width / 2f, rect.Y + rect.Height / 2f);
+                var r = Math.Min(rect.Height, rect.Width) / 2;
+                var points = new PointF[_edgesCount];
+                for (int i = 0; i < points.Length; i++)
+                {
+                    points[i] = new PointF(pt.X + r * (float)Math.Cos(_angle +2 * Math.PI * i / _edgesCount),
+                        pt.Y + r * (float)Math.Sin(_angle +2 * Math.PI * i / _edgesCount));
+                }
+                path.AddPolygon(points);
+                Region = new Region(path);
+            }
+        }
     }
 }
--- Durak/Settings.Designer.cs
+++ Durak/Settings.Designer.cs
@@ -32,11 +32,22 @@ namespace Durak
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.lblSettings = new System.Windows.Forms.Label();
+            this.lblSuit = new System.Windows.Forms.Label();
+            this.lblTable = new System.Windows.Forms.Label();
+            this.picBoxSuit = new System.Windows.Forms.PictureBox();
+            this.picBoxTable = new System.Windows.Forms.PictureBox();
+            this.btnTableRight = new Durak.Classes.TriangleButton();
+            this.btnTableLeft = new Durak.Classes.TriangleButton();
+            this.btnSuitLeft = new Durak.Classes.TriangleButton();
+            this.btnSuitRight = new Durak.Classes.TriangleButton();
+            ((System.ComponentModel.ISupportInitialize) (this.picBoxSuit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.picBoxTable)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(382, 383);
+            this.button1.Location = new System.Drawing.Point(183, 382);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,16 +55,129 @@ namespace Durak
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // lblSettings
+            // 
+            this.lblSettings.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.lblSettings.Location = new System.Drawing.Point(155, 9);
+            this.lblSettings.Name = "lblSettings";
+            this.lblSettings.Size = new System.Drawing.Size(133, 31);
+            this.lblSettings.TabIndex = 1;
+            this.lblSettings.Text = "SETTINGS";
+            // 
+            // lblSuit
+            // 
+            this.lblSuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.lblSuit.Location = new System.Drawing.Point(38, 124);
+            this.lblSuit.Name = "lblSuit";
+            this.lblSuit.Size = new System.Drawing.Size(100, 23);
+            this.lblSuit.TabIndex = 2;
+            this.lblSuit.Text = "Suit";
+            // 
+            // lblTable
+            // 
+            this.lblTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.lblTable.Location = new System.Drawing.Point(38, 280);
+            this.lblTable.Name = "lblTable";
+            this.lblTable.Size = new System.Drawing.Size(100, 23);
+            this.lblTable.TabIndex = 3;
+            this.lblTable.Text = "Table";
+            // 
+            // picBoxSuit
+            // 
+            this.picBoxSuit.Location = new System.Drawing.Point(322, 85);
+            this.picBoxSuit.Name = "picBoxSuit";
+            this.picBoxSuit.Size = new System.Drawing.Size(80, 110);
+            this.picBoxSuit.TabIndex = 4;
+            this.picBoxSuit.TabStop = false;
+            // 
+            // picBoxTable
+            // 
+            this.picBoxTable.Location = new System.Drawing.Point(322, 241);
+            this.picBoxTable.Name = "picBoxTable";
+            this.picBoxTable.Size = new System.Drawing.Size(80, 110);
+            this.picBoxTable.TabIndex = 5;
+            this.picBoxTable.TabStop = false;
+            // 
+            // btnTableRight
+            // 
+            this.btnTableRight.Angle = 0F;
+            this.btnTableRight.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnTableRight.EdgesCount = 3;
+            this.btnTableRight.Location = new System.Drawing.Point(408, 265);
+            this.btnTableRight.Name = "btnTableRight";
+            this.btnTableRight.Size = new System.Drawing.Size(32, 61);
+            this.btnTableRight.TabIndex = 6;
+            this.btnTableRight.UseVisualStyleBackColor = false;
+            // 
+            // btnTableLeft
+            // 
+            this.btnTableLeft.Angle = 66F;
+            this.btnTableLeft.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnTableLeft.EdgesCount = 3;
+            this.btnTableLeft.Location = new System.Drawing.Point(284, 265);
+            this.btnTableLeft.Name = "btnTableLeft";
+            this.btnTableLeft.Size = new System.Drawing.Size(32, 61);
+            this.btnTableLeft.TabIndex = 7;
+            this.btnTableLeft.UseVisualStyleBackColor = false;
+            // 
+            // btnSuitLeft
+            // 
+            this.btnSuitLeft.Angle = 66F;
+            this.btnSuitLeft.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSuitLeft.EdgesCount = 3;
+            this.btnSuitLeft.Location = new System.Drawing.Point(284, 109);
+            this.btnSuitLeft.Name = "btnSuitLeft";
+            this.btnSuitLeft.Size = new System.Drawing.Size(32, 61);
+            this.btnSuitLeft.TabIndex = 8;
+            this.btnSuitLeft.UseVisualStyleBackColor = false;
+            // 
+            // btnSuitRight
+            // 
+            this.btnSuitRight.Angle = 0F;
+            this.btnSuitRight.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSuitRight.EdgesCount = 3;
+            this.btnSuitRight.Location = new System.Drawing.Point(408, 109);
+            this.btnSuitRight.Name = "btnSuitRight";
+            this.btnSuitRight.Size = new System.Drawing.Size(32, 61);
+            this.btnSuitRight.TabIndex = 9;
+            this.btnSuitRight.UseVisualStyleBackColor = false;
+            // 
             // Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(465, 437);
+            this.Controls.Add(this.btnSuitRight);
+            this.Controls.Add(this.btnSuitLeft);
+            this.Controls.Add(this.btnTableLeft);
+            this.Controls.Add(this.btnTableRight);
+            this.Controls.Add(this.picBoxTable);
+            this.Controls.Add(this.picBoxSuit);
+            this.Controls.Add(this.lblTable);
+            this.Controls.Add(this.lblSuit);
+            this.Controls.Add(this.lblSettings);
             this.Controls.Add(this.button1);
             this.Name = "Settings";
             this.Text = "Settings";
+            ((System.ComponentModel.ISupportInitialize) (this.picBoxSuit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.picBoxTable)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private Durak.Classes.TriangleButton btnSuitLeft;
+        private Durak.Classes.TriangleButton btnSuitRight;
+
+        private Durak.Classes.TriangleButton btnTableLeft;
+
+        private Durak.Classes.TriangleButton btnTableRight;
+
+        private System.Windows.Forms.PictureBox picBoxSuit;
+        private System.Windows.Forms.PictureBox picBoxTable;
+
+        private System.Windows.Forms.Label lblTable;
+
+        private System.Windows.Forms.Label lblSettings;
+        private System.Windows.Forms.Label lblSuit;
 
         private System.Windows.Forms.Button button1;
 
